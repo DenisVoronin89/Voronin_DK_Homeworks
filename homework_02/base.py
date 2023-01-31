e57a92e0d5c2aaf82931585d8540867479156ea3
Denis Voronin <kaiklayton@bk.ru>
2023-01-31 15:21:38 +0300

    Fix move function errors

--- homework_02/base.py
+++ homework_02/base.py
@@ -14,7 +14,6 @@ class Vehicle(ABC):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consuption
-        return
 
 
     def start(self):
@@ -25,15 +24,11 @@ class Vehicle(ABC):
                 raise LowFuelError("Error - Low fuel")
 
 
-
     def move(self, distance):
-        quantity = distance // self.fuel_consumption   # check the amount of fuel to overcome a given distance
-        if self.fuel > 0 and quantity <= self.fuel:
-            self.fuel -= quantity
-        else:
-            raise NotEnoughFuel("Error - Not enough fuel")
-
-
-
+           amount_of_fuel = self.fuel_consumption * distance # check the amount of fuel to overcome a given distance
+           if self.fuel >= amount_of_fuel:
+               self.fuel -= amount_of_fuel
+           else:
+               raise NotEnoughFuel("Error - Not enough fuel")
 
 
