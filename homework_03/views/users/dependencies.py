798910ccf3531c539a41a82993f02fda86262256
Denis Voronin <kaiklayton@bk.ru>
2023-02-22 10:44:53 +0400

    add package "users" (crud, schemas, swagger_views, dependencies, __init__.py modules)

---
+++ homework_03/views/users/dependencies.py
@@ -0,0 +1,20 @@
+"""
+Depence for swagger_views.py
+"""
+
+
+from fastapi import Header, HTTPException, status
+
+from .crud import get_user_by_token
+from .schemas import User
+
+def user_by_token(x_auth_token: str = Header()) -> User:
+    user = get_user_by_token(x_auth_token)
+    if user:
+        return user
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Auth Token invalid!'
+    )
+
