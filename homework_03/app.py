2e61614ccf6f36af0b8df9767831c1be0367cebd
Denis Voronin <kaiklayton@bk.ru>
2023-02-22 10:40:32 +0400

    add get('/ping) and get('/hello') requests

--- homework_03/app.py
+++ homework_03/app.py
@@ -8,3 +8,19 @@ def root():
     data = "<h2>*** WELCOME TO HOMEWORK 03 ***</h2>"
     return HTMLResponse(content=data)
 
+
+@app.get('/ping')
+def ping_message():
+    return Response(
+        status_code = status.HTTP_200_OK,
+        content=json.dumps({"message": "pong"}),
+        media_type="application/json",
+    )
+
+
+@app.get('/hello')
+def hello(name: str):
+    if name is None:
+        name = "User"
+    data = f"Hello {name}!"
+    return Response(content=data, media_type="text/plain")
